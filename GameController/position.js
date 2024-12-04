37307b0085bd4c4fb81d9d43cfe834bd697edf60
TeamA1-GP <TeamA1@GaryPedretti.com>
2024-12-04 22:07:34 +0000

    Showing my fleet and enemy fleet hits/misses. (Doesn't have misses yet. Will add once miss tracker is in.)

--- GameController/position.js
+++ GameController/position.js
@@ -8,6 +8,13 @@ class Position {
         return this.column.toString() + this.row.toString()
     }
 
+    equals(otherPosition) {
+        if (otherPosition.row == this.row && otherPosition.column == this.column)
+            return true;
+
+        return false;
+    }
+
 }
 
 module.exports = Position;
