c2bdef782b62a45e6181ac75de143e06362c106e
JohnM <Johnm@wilcoxind.com>
2024-12-04 16:35:08 +0000

    Added PBI 2, AC 1

--- GameController/ship.js
+++ GameController/ship.js
@@ -4,10 +4,29 @@ class Ship {
         this.size = size;
         this.color = color;
         this.positions = [];
+        this.hits = [];
+        this.sunk =  false;
     }
 
     addPosition(position) {
         this.positions.push(position);
+    }
+
+    addHit(position) {
+        this.hits.forEach( hit => {
+            if( hit == position )
+            {
+                console.log("Duplicate found")
+                return;
+            }
+        });
+        console.log("Hit pushed")
+        this.hits.push(position);
+
+        if( this.hits.length == this.size ){
+            console.log(`${ship.name} Sunk` )
+            this.sunk = true;
+        }
     }
 }
 
